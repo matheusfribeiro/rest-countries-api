40a626fbe58e425183ddba5c5009d0de77ea6aba
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-02-25 20:38:59 -0300

    feat: grid display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-console.log('test for echo')
+let http = new XMLHttpRequest()
+
+http.open('get', 'data.json', true)
+
+http.send()
+
+http.onload = function() {
+
+  if (this.readyState == 4 && this.status == 200) {
+    let countries = JSON.parse(this.responseText)
+
+    let output = ''
+
+    for (let country of countries) {
+      
+    }
+  }
+
+
+}
