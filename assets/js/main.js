19350c85a93ab31e1ea80990e36dd068143f5d23
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-02-26 18:41:43 -0300

    feat: filtered search

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,38 @@
-const countryDiv = document.querySelector('.countries')
+const countryList = document.querySelector('.countries')
+let searchInput = document.querySelector('#input-country')
 
 import countries from './data.json' assert {type: 'json'}
 
 countries.forEach((country) => {
   const {name, population, region, capital} = country
 
-  countryDiv.innerHTML += `
+  countryList.innerHTML += `
       <div class ="country">
-        <div class="flag"></div>
+        <img src="${country.flags.png}" class="flag"></img>
         <div class="info">
         <h5>${name}</h5>
-        <p>Population: ${population}</p>
-        <p>Region: ${region}</p>
-        <p>Capital: ${capital}</p>
+        <p><b>Population:</b> ${population}</p>
+        <p><b>Region:</b> ${region}</p>
+        <p><b>Capital:</b> ${capital}</p>
       </div>
   `
+  
 })
+
+searchInput.addEventListener('input', () => {
+  const filter = searchInput.value.toLowerCase()
+  const listCountries = document.querySelectorAll('.country')
+  
+  listCountries.forEach((country) => {
+    let text = country.lastChild.previousSibling.childNodes[1].innerHTML
+    if (text.toLowerCase().includes(filter.toLowerCase())) {
+      country.style.display = ''
+    } else {
+      country.style.display = 'none'
+    }
+    
+  })
+})
+
+
+
